21368b5261f8896f7e57d6a0d6b72a4f87880458
yutopp <yutopp@gmail.com>
2013-03-26 19:36:50 +0900

    init

--- benri.rb
+++ benri.rb
@@ -5,14 +5,15 @@ require 'fileutils'
 SITE_DIR = '_site'
 FOR_GH_DIR = '__deploy'
 
-REPO = 'git@github.com:yutopp/gh-test.git'
-REPO_NAME = 'gh-test'
+REPO = 'git@github.com:dartrefjp/dartrefjp.github.com.git'
+REPO_NAME = 'dartrefjp.github.com'
 
 BRANCH_GH_PAGES = 'gh-pages'
 
+
 Dir.chdir( File.dirname(__FILE__) ) do
   p "at #{Dir.pwd}"
-  
+
   case ARGV[0]
   #
   when 'setup'
@@ -25,19 +26,19 @@ Dir.chdir( File.dirname(__FILE__) ) do
         begin
           system "git checkout -B #{BRANCH_GH_PAGES}"
           system "git rm -rf *"
-          
+
           Dir.entries( Dir.pwd ).each do |f|
             FileUtils.remove_entry_secure f unless f == '.git' || f == '.' || f == '..'
           end
-          
+
           FileUtils.touch '.nojekyll'
-          
+
           Dir.entries( Dir.pwd ).each do |f|
             p "-> #{f}"
           end
-          
+
           system "git commit -m \"init\""
-          
+
         ensure
           #system "git checkout #{current_branch}"
         end
@@ -49,19 +50,19 @@ Dir.chdir( File.dirname(__FILE__) ) do
     # build
     system 'jekyll'
     src_dir = Dir.pwd + '/' + SITE_DIR
-    
+
     Dir.chdir( FOR_GH_DIR ) do
       Dir.chdir( REPO_NAME ) do
         dst_dir = Dir.pwd
         p "from #{src_dir} -> to #{dst_dir}"
-        
+
         current_branch = File.basename( `git symbolic-ref HEAD 2> /dev/null` )
         begin
           system "git checkout #{BRANCH_GH_PAGES}"
-          
+
           system "git reset"
           system "git rm -rf *"
-          
+
           # copy site data
           Dir.entries( src_dir ).select{|p| p != '.' && p != '..'}.map{|p| "#{src_dir}/#{p}"}.each{|f| p f}
           FileUtils.cp_r( Dir.entries( src_dir ).select{|p| p != '.' && p != '..'}.map{|p| "#{src_dir}/#{p}"}, dst_dir )
@@ -70,7 +71,7 @@ Dir.chdir( File.dirname(__FILE__) ) do
           system "git add ."
           system "git commit -m \"generate\""
           system "git push -f origin #{BRANCH_GH_PAGES}"
-          
+
         ensure
           #system "git checkout #{current_branch}"
         end
@@ -80,14 +81,14 @@ Dir.chdir( File.dirname(__FILE__) ) do
   #
   when 'delete'
     system "git push origin :#{BRANCH_GH_PAGES}"
-    
+
   #
   when 'server'
     system "jekyll _dev_site/ --server --url http://localhost:4000"
-    
+
   else
     p "Use -> ./benri (setup|deploy|delete|server)"
-    
+
   end
-  
+
 end # chdir
